5636ea8b7be12db1cc590c3290f5f1017171ab06
Murillo Caetano <murillo.santos@ucsal.edu.br>
2022-03-25 10:18:04 -0300

    first commit

---
+++ src/main/java/com/mtz/conexaosaude/conexaosaude/ConexaoSaudeApplication.java
@@ -0,0 +1,13 @@
+package com.mtz.conexaosaude.conexaosaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConexaoSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConexaoSaudeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtz/conexaosaude/conexaosaude/ConexaoSaudeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtz.conexaosaude.conexaosaude;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConexaoSaudeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
